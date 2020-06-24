b4f535fa08cce7ee2228a9f6a158b16d411ddb7a
Suvarna Chetty <suvarnachetty6@gmail.com>
2020-06-24 09:08:04 +0200

    Restarting over

--- test/pages/eda.py
+++ test/pages/eda.py
@@ -1,6 +1,1 @@
-import streamlit as st
-import requests
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import joblib,os
+
--- test/pages/mission.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import requests
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import joblib,os
